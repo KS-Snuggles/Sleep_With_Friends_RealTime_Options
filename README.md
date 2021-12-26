d309a79cdd474ab76494de482ebfe0278873905c
Snuggles <96385981+KS-Snuggles@users.noreply.github.com>
2021-12-26 12:47:02 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Sleep_With_Friends_RealTime_Options
-Sleep With Friends branch to have users choose real time minutes instead of in-game hours
+# Project Zomboid - Mod - Sleep With Friends
+A mod for the game Project Zomboid that gives back the possibility to sleep in multiplayer without having to wait hours in front of your computer.
+
+# Steam Workshop URL
+https://steamcommunity.com/sharedfiles/filedetails/?id=2686624983
---
+++ media/lua/client/SleepWithFriendsRealTimeSettings.lua
@@ -0,0 +1,29 @@
+local function mpSleep()
+	local player = getPlayer()
+	if player:isAsleep() then
+		if  LoopCheck == 1 then
+			local modFatigue = (modFatigue - subFatThree)
+			local modEndurance = (modEndurance + addEndur)
+			player:getStats():setFatigue(modFatigue)
+			player:getStats():setEndurance(modEndurance)
+			if player:getStats():getFatigue() <= 0 and AutoWake == 1 then
+				local LoopCheck = 0
+				player:forceAwake()
+			elseif player:getStats():getFatigue() <= 0 and AutoWake == 2 then
+				player:getStats():setFatigue(0)
+			else
+			end
+		else
+			local serverDayLength = (SandboxVars.DayLength - 2)
+			local modFatigue = player:getStats():getFatigue()
+			local modEndurance = player:getStats():getEndurance()
+			local AutoWake = SandboxVars.SleepWithFriends.AutoWake
+			local subFatOne = (24 / serverDayLength)
+			local subFatTwo = (1 / subFatOne)
+			local subFatThree = (subFatTwo / SandboxVars.SleepWithFriends.TestOption)
+			local addEndur = (subFatThree * 2)
+			local LoopCheck = 1
+		end
+	end
+end
+Events.EveryOneMinute.Add(mpSleep)
